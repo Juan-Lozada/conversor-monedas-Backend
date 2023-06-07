3efc43c72c726e3c3d35a13c3693cf87dd3d2cbc
Juan <juan.lozada1905@gmail.com>
2023-06-06 23:07:32 -0400

    prueba

--- src/indexRoute/indexRoute.js
+++ src/indexRoute/indexRoute.js
@@ -14,7 +14,15 @@ router.post("/login", indexController.iniciarSesion);
 router.get("/", (req, res) => {
   res.send('Welcome to my Server')
 })
-
+router.put("/usuario/:id", indexController.editarUsuario);
+router.post("/registro/recinto", indexController.registroRecinto);
+router.post("/reserva", indexController.registroReserva);
+router.post("/registro/cancha", indexController.registroCancha);
+router.get("/recintos/tenant", indexController.getRecintosTenant);
+router.get("/recintos/usuario", indexController.getRecintosUser);
+router.get("/canchas", indexController.getCanchas);
 router.get("/usuario/:id", indexController.getUser);
+router.put("/recinto/:id", indexController.editarRecinto);
+router.put("/cancha/:id", indexController.editarCancha);
 
 module.exports = router;
